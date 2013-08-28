9e2670379e64d611405104383c4184d3efebf8c6
Andrew F. Dreher <afdreher@agavix.com>
2013-08-28 14:05:23 -0500

    Code cleanup to use constants; adds comments to define code sections

--- DayFlow/DFDatePickerViewController.m
+++ DayFlow/DFDatePickerViewController.m
@@ -1,44 +1,72 @@
 #import "DFDatePickerViewController.h"
 
+//
+// CONSTANTS
+//
+
+NSString * const SELECTED_DATE_KEYPATH = @"datePickerView.selectedDate";
+
+
 @implementation DFDatePickerViewController
 @synthesize datePickerView = _datePickerView;
 
-- (void) viewDidLoad {
+//
+//  VIEW LIFECYCLE
+//
+
+- (void)viewDidLoad {
 	[super viewDidLoad];
 	[self.view addSubview:self.datePickerView];
 }
 
-- (DFDatePickerView *) datePickerView {
-	if (!_datePickerView) {
-		_datePickerView = [DFDatePickerView new];
-		_datePickerView.frame = self.view.bounds;
-		_datePickerView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-	}
-	return _datePickerView;
-}
-
-- (void) viewDidAppear:(BOOL)animated {
+- (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
-	[self addObserver:self forKeyPath:@"datePickerView.selectedDate" options:NSKeyValueObservingOptionOld|NSKeyValueObservingOptionNew context:(void *)self];
+	[self addObserver:self
+         forKeyPath:SELECTED_DATE_KEYPATH
+            options:NSKeyValueObservingOptionOld|NSKeyValueObservingOptionNew
+            context:(void *)self];
 }
 
-- (void) viewWillDisappear:(BOOL)animated {
+- (void)viewWillDisappear:(BOOL)animated {
 	[super viewWillDisappear:animated];
 
   id observationInfo = [self observationInfo];
   if (observationInfo != nil) {
-    [self removeObserver:self forKeyPath:@"datePickerView.selectedDate" context:(void *)self];
+    [self removeObserver:self forKeyPath:SELECTED_DATE_KEYPATH context:(void *)self];
   }
 }
 
-- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-	if ([keyPath isEqualToString:@"datePickerView.selectedDate"]) {
+
+//
+//  KEY VALUE OBSERVING
+//
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+	if ([keyPath isEqualToString:SELECTED_DATE_KEYPATH]) {
 		NSDate *toDate = change[NSKeyValueChangeNewKey];
 		if ([toDate isKindOfClass:[NSDate class]]) {
 			//	toDate might be NSNull
 			[self.delegate datePickerViewController:self didSelectDate:toDate];
 		}
 	}
+}
+
+
+//
+//  PROPERTY METHODS
+//
+
+- (DFDatePickerView *)datePickerView {
+	if (!_datePickerView) {
+		_datePickerView = [DFDatePickerView new];
+		_datePickerView.frame = self.view.bounds;
+		_datePickerView.autoresizingMask =
+    UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+	}
+	return _datePickerView;
 }
 
 @end
